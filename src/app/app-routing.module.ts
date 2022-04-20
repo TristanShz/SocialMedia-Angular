7e9c7693eeb65e64424883177f28c52257442a34
TristanSch1 <tristan.sch57@gmail.com>
2022-04-20 23:03:06 +0200

    Added template for login page, homepage, article page and user list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component'
+import { LoginComponent } from './login/login.component'
+import { HomeComponent } from './home/home.component'
+import { ArticleComponent } from './article/article.component'
 
 const routes: Routes = [
-  { path: "", component: RegisterComponent}
+  { path: "", component: RegisterComponent},
+  { path: "login", component: LoginComponent},
+  { path: "home", component: HomeComponent},
+  { path: "article", component: ArticleComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { ArticlePreviewComponent } from './article-preview/article-preview.component';
+import { ArticleComponent } from './article/article.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    SideMenuComponent,
+    ArticlePreviewComponent,
+    ArticleComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
