c0f0bbe1f788e2c9041f2fdca4b681bcbf6c817d
TristanSch1 <tristan.sch57@gmail.com>
2022-04-20 23:20:48 +0200

    Starting to use controller for article data

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlePreviewComponent implements OnInit {
 
-  constructor() { }
+  articleList:{title: string, content: string}[]
+  constructor() {
+    this.articleList = [
+      {
+        title: "Title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed" +
+          "      do eiusmod tempor incididunt ut labore et dolore" +
+          "      magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+          "      exercitation ullamco laboris nisi ut aliquip ex ea" +
+          "      commodo consequat. Duis aute irure dolor in reprehenderit in" +
+          "      voluptate velit esse cillum dolore eu fugiat nulla pariatur..."
+      },
+
+      {
+        title: "Title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed" +
+          "      do eiusmod tempor incididunt ut labore et dolore" +
+          "      magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+          "      exercitation ullamco laboris nisi ut aliquip ex ea" +
+          "      commodo consequat. Duis aute irure dolor in reprehenderit in" +
+          "      voluptate velit esse cillum dolore eu fugiat nulla pariatur..."
+      },
+      {
+        title: "Title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed" +
+          "      do eiusmod tempor incididunt ut labore et dolore" +
+          "      magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+          "      exercitation ullamco laboris nisi ut aliquip ex ea" +
+          "      commodo consequat. Duis aute irure dolor in reprehenderit in" +
+          "      voluptate velit esse cillum dolore eu fugiat nulla pariatur..."
+      },
+    ]
+  }
 
   ngOnInit(): void {
   }
